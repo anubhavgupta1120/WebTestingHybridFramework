c0e76bddaeb37d7285bc0a056642cf85ea25c327
anubhavgupta1120 <anubhavgp11@gmail.com>
2023-10-28 18:26:12 +0530

    New Project

---
+++ src/main/java/com/WebTesting/WebTesting_Hybrid_Framework/App.java
@@ -0,0 +1,13 @@
+package com.WebTesting.WebTesting_Hybrid_Framework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
